727c7aeb09ff58d9594f9eba845f5f81e7e09d3f
marco <mtgiabbani@gmail.com>
2022-10-26 17:08:43 -0300

    adds some styles

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -89,7 +89,7 @@ export default function Home (){
 
                     <li className="nav-item">
                         <div className="nav-link" onClick={event => {handleClick(event)}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="mini-logo">
                                 <path d="M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z"/>
                             </svg>
                             <span className="link-text">Reload dogs</span>
